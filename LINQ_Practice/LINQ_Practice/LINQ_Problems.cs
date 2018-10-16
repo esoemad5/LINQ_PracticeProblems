e9054820f140f8edbc379e28e79e62417927ca2a
esoemad5 <esoemad5@gmail.com>
2018-10-16 09:47:49 -0500

    Finished problem 2.

---
+++ LINQ_Practice/LINQ_Practice/LINQ_Problems.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Practice
+{
+    class LINQ_Problems
+    {
+
+        public static void Problem2()
+        {
+            List<string> names = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike" };
+            // Return a copy without duplicates
+            var noDupes = names.Select(n => n).Distinct();
+
+            foreach(var item in noDupes)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
